c3024dabd63aed169ad4a0135239295a6ef96992
Abimanyu RS <45155771+Abimanyu007@users.noreply.github.com>
2021-10-19 17:47:13 +0530

    Item Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Item from './MyItem';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <Item/>
+
       </header>
     </div>
   );
