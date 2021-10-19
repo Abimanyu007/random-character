02664823e178c4889217c12e25b8d9b88e09cd0a
Abimanyu RS <45155771+Abimanyu007@users.noreply.github.com>
2021-10-19 18:32:11 +0530

    States & Props

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     <Item name="Abimanyu" />
-
+     <Item name="Cat" />
+     <Item name="Dog" />
+     <Item name="Cow" />
       </header>
     </div>
   );
--- src/MyItem.js
+++ src/MyItem.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 class Item extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            clicked:0,
+            totalRemaining:10
+        }
+
+    }
+
+    ClickMe(){
+            this.setState({clicked:this.state.clicked+1,
+            totalRemaining:this.state.totalRemaining-1})
+
+    }
+
     render() {
       return (
-        <h1>Hello, My name is {this.props.name}</h1>
+          <div>
+        <h1 onClick={() => this.ClickMe()}>Hello, I'm a  {this.props.name}</h1>
+      
+      <span>
+          <p>{this.state.clicked} is the no of times i clicked {this.props.name} , Remaining {this.state.totalRemaining}</p>
+      </span>
+</div>
       )
     }
   
