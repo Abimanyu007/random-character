de40a744dd74d6e3c28bc7fe5b5f0b92b3b0e2b5
Abimanyu RS <45155771+Abimanyu007@users.noreply.github.com>
2021-10-19 19:50:56 +0530

    Base works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,72 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Item from './MyItem';
+// import Item from './MyItem';
+import React from 'react';
 
+class StarWars extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      Name: null,
+      Height: null,
+      Homeworld: null,
+      Movies: [],
+      loadedcharacter: false
+    }
+
+
+  }
+
+  RandomCharacterGenerator(){
+  
+
+    this.setState({
+      Name: 'Luke',
+      Height: 174,
+      Homeworld:'Tatooine',
+      Movies: ["item 1", "item 2", "item 3"],
+      loadedcharacter:true
+    })
+  }
+
+render() {
+  return (
+    <div>
+    {
+      this.state.loadedcharacter && 
+      <div>
+      <h1>{this.state.Name}</h1>
+      <p>{this.state.Height}</p>
+      <p>{this.state.Homeworld}</p>
+      <ul>
+        <li>{this.state.Movies}</li>
+      </ul>
+      </div>
+
+    }
+  
+
+      <button className="btn" onClick={() => this.RandomCharacterGenerator()}>Generate!</button>
+
+    
+
+    </div>
+  )
+}
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-     <Item name="Cat" />
+
+    <StarWars />
+     {/* <Item name="Cat" />
      <Item name="Dog" />
-     <Item name="Cow" />
+     <Item name="Cow" /> */}
+
       </header>
     </div>
   );
